a688d1b47e14304ed42fc3ddb58777b6c3b684d8
adorable-otter <plan.stack@gmail.com>
2025-02-15 20:41:54 +0900

    style: 링크간 간격 조정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,9 +5,15 @@ const Home = () => {
     <div>
       <h1 className="form-title">Home</h1>
       <div className="flex gap-10">
-        <Link to={'/mypage'}>마이페이지</Link>
-        <Link to={'/login'}>로그인</Link>
-        <Link to={'/signup'}>회원가입</Link>
+        <Link className="p-2" to={'/mypage'}>
+          마이페이지
+        </Link>
+        <Link className="p-2" to={'/login'}>
+          로그인
+        </Link>
+        <Link className="p-2" to={'/signup'}>
+          회원가입
+        </Link>
       </div>
     </div>
   );
